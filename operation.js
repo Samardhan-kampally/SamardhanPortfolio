73595ffaf904903f68ef04b186fb6b59296ca884
Samardhan-anarghya <samardhan@anarghyacomm.in>
2025-03-05 01:04:17 +0530

    portfolio

---
+++ operation.js
@@ -0,0 +1,46 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+var tablink;
+var tabcontent;
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.left = "0";
+}
+function closemenu() {
+  sidemenu.style.left = "-200px";
+}
+
+///////////////
+
+const scriptURl =
+  "https://script.google.com/macros/s/AKfycbwrBcpHwrZhe76JlqF-el5GpUd-_4CoPupE7yAyS6cWIIDy-PCD_TG3to9senbwOF_4/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURl, { method: "POST", body: new FormData(form) })
+    .then((r) => {
+      msg.innerHTML = "Message Sent Successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((e) => console.error("error", e.message));
+});
