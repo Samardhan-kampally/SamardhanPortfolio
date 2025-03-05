c95424517692c84f14ae7b0de06d387514164323
Samardhan-anarghya <samardhan@anarghyacomm.in>
2025-03-06 01:36:05 +0530

    6/3/25

--- operation.js
+++ operation.js
@@ -25,6 +25,15 @@ function closemenu() {
   sidemenu.style.left = "-200px";
 }
 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 ///////////////
 
 const scriptURl =
